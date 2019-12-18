0709868c12731f7c63760dd8354fc1bb96583aa4
SICHE <clement.siche@gmail.com>
2019-12-18 15:15:19 +0100

    init component and page for the product detail

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductDetailsPageComponent } from './pages/product-details-page/product-details-page.component';
+import { UserReviewComponent } from './components/user-review/user-review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDetailsPageComponent,
+    UserReviewComponent
   ],
   imports: [
     BrowserModule,
