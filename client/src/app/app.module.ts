96470b002965c3463231d5f2217e02428fa59efd
SICHE <clement.siche@gmail.com>
2019-12-19 09:29:08 +0100

    HTML product detail comp

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppComponent } from './app.component';
 import { ProductDetailsPageComponent } from './pages/product-details-page/product-details-page.component';
 import { UserReviewComponent } from './components/user-review/user-review.component';
 import { CreateComponent } from './pages/create/create.component';
-
-import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
